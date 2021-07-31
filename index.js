c5ab3b480f83a42692d24690524a8e9f4b035bb3
lakshanamal <lakshanamal100@gmail.com>
2021-07-31 23:47:01 +0530

    connect mysql

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
 
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "node",
+});
+
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("mysql conntecd");
+  }
+});
 app.get("/", (req, res) => {
   res.send("hello world");
 });
